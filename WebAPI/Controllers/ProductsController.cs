6391b8c50de5b777b375c01071473320da6da8eb
sametion <sametacikk@gmail.com>
2024-07-26 16:17:54 +0300

    webApi  controller built
    configuration continued

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //Attribute [] like java's annotation
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+        // loosely coupled
+        //IoC container--  inversion of control
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getAll")]
+        public IActionResult GetAll()
+        {
+            var result = _productService.GetAll();
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+        }
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.IsSuccess)
+            
+                {
+                    return Ok(result);
+                }
+           return BadRequest(result);
+
+
+        }
+        [HttpGet("getById")]
+        public IActionResult GetById(int id)
+        {
+            var result=_productService.GetById(id);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
