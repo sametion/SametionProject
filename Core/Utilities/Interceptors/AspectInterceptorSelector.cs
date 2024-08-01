4812115a2a22644dddd6a0611768f6f1a9282415
sametion <sametacikk@gmail.com>
2024-08-01 17:43:46 +0300

    autofac config
    ıoc autofac business validator etc

---
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -0,0 +1,30 @@
+﻿using Castle.DynamicProxy;
+using Microsoft.Build.Logging;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static Core.Utilities.Interceptors.AspectInterceptorSelector;
+using IInterceptor = Castle.DynamicProxy.IInterceptor;
+
+namespace Core.Utilities.Interceptors
+{
+
+    public partial class AspectInterceptorSelector : IInterceptorSelector
+    {
+        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+        {
+            var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>
+                (true).ToList();
+            var methodAttributes = type.GetMethod(method.Name)
+                .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
+            classAttributes.AddRange(methodAttributes);
+           
+
+            return classAttributes.OrderBy(x => x.Priority).ToArray();
+        }
+    }
+}
